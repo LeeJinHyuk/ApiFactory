c11fc7532c1a6fe0e966eacdb24ff31b58733620
LeeJinHyuk <eerto@naver.com>
2017-08-17 15:47:16 +0900

    update data request logic - xml2js 모듈 설치 및 적용 - xml 데이터를 json 으로 파싱하여 전달하는 로직 추가

--- source/functions/index.js
+++ source/functions/index.js
@@ -1,5 +1,40 @@
 const request = require("request");
+const xml2js = require('xml2js');
 
-exports.handler = (event, context, callback) => {
-    callback(null, {result : "0", data : "pppp"});
+exports.handler = (_event, _context, _callback) => {
+    let url = "http://openapi.youth.go.kr/openapi/service/YouthActivInfoCertiSrvc/";
+    let key = "HF1eUr96KfQkuZe3Pl1v0stWJvCU8eH72E%2BPGfe%2BiUOMDUlk0P1%2FMgO4SpXf0qq74hzOF7ctuBDJl2L7aXXOsw%3D%3D";
+    let path = _event.params.path;
+    let apiPath = path.method;
+    let options = {
+        url : url + apiPath + "?serviceKey=" + key + "&numOfRows=10",
+        method : "GET",
+        timeout : 10 * 1000
+    };
+
+    switch(apiPath) {
+        case "getCertiProgrmList" :
+        case "getVolProgrmList" :
+        case "getYngbgsIntrlExchgProgrmList" :
+            options.url = options.url + "&numOfRows=" + path.count;
+            break;
+    }
+
+    request(options, (_reqError, _response, _body) => {
+        let statusCode = _response.statusCode;
+        let parser = new xml2js.Parser();
+        let result = {
+            statusCode : statusCode
+        };
+
+        if (statusCode === 200) {
+            parser.parseString(_body, (_parserErr, _result) => {
+                result.data = _result;
+            });
+        } else {
+            result.data = {};
+        }
+
+        _callback(null, result);
+    });
 };
